ae3ab190aa7e47255162979026a560d72e327247
Conor McKnight <C0nw0nks@googlemail.com>
2023-06-17 21:33:18 +0100

    Update selenium.cmd

--- selenium.cmd
+++ selenium.cmd
@@ -140,7 +140,7 @@ set debug=0
 :: means you do not need to edit the batch file you can focus on your selenium script modifications without needing to escape batch code
 :: 1 enabled
 :: 0 disabled
-set custom_selenium_script=0
+set custom_selenium_script=1
 
 ::Set custom user-agent to use if you do not want to expose your default selenium browser user-agent
 set custom_user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52"
